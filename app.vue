f0c376913a9d07e0841ef2f690973e50f92d7f34
pong7772 <roth.samnangvisothipong19@kit.edu.kh>
2022-09-04 10:39:54 +0700

    test

--- app.vue
+++ app.vue
@@ -1,7 +1,7 @@
 <template>
   <div :class="{ dark: darkMode }">
     <!-- <NuxtWelcome /> -->
-    <h1 class="text-red-500 dark:text-dim-500">Hello Pong</h1>
+    <h1 class="text-red-500 dark:text-dim-500">Hello Pofdgadfng</h1>
   </div>
 </template>
 
