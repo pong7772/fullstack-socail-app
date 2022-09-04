9c37ae6f017e23913e0efead692f38b9433db1d3
Visothipong RoTH <administrator@VisothipongMacBook.local>
2022-09-04 09:41:01 +0700

    First commit 🚀

---
+++ app.vue
@@ -0,0 +1,10 @@
+<template>
+  <div :class="{ dark: darkMode }">
+    <!-- <NuxtWelcome /> -->
+    <h1 class="text-red-500 dark:text-dim-500">Hello Pong</h1>
+  </div>
+</template>
+
+<script setup>
+const darkMode = ref(true);
+</script>
