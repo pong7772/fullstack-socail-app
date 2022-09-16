5bcfda8517a70cff1aff9693f57fc0d64d56457c
pong7772 <roth.samnangvisothipong19@kit.edu.kh>
2022-09-16 15:06:37 +0700

    day1 leftsidebar

--- app.vue
+++ app.vue
@@ -13,12 +13,15 @@
           "
         >
           <!-- Left sidebar -->
-          <div class="md:block xs-col-span-1 xl:col-span-2">
+          <div class="hidden md:block xs-col-span-1 xl:col-span-2">
             <div class="sticky top-0">
               <SidebarLeft />
             </div>
           </div>
           <!-- Center sidebar -->
+          <main class="col-span-12 md:col-span-8 xl:col-span-6 bg-red-50">
+            <p>sflashf</p>
+          </main>
 
           <!-- Right sidebar -->
         </div>
---
+++ components/Sidebar/Left/Tab.vue
@@ -0,0 +1,41 @@
+<template>
+  <div>
+    <nuxt-link
+      to="/"
+      class="
+        flex
+        items-center
+        text-black
+        p-3
+        w-min
+        hover:bg-gray-200
+        rounded-full
+        dark:hover:bg-dim-200 dark:text-white
+      "
+      :class="deafaultTransitions"
+    >
+      <div class="w-6 h-6 text-dark">
+        <slot name="icon"></slot>
+      </div>
+      <div class="ml-4 text-xl hidden xl:block" :class="txtClasses">
+        <slot name="name"></slot>
+      </div>
+    </nuxt-link>
+  </div>
+</template>
+
+<script setup>
+import useTailwindConfig from "~~/components/composables/useTailwindConfig";
+
+const { deafaultTransitions } = useTailwindConfig();
+const props = defineProps({
+  active: {
+    type: Boolean,
+    default: false,
+  },
+});
+
+const txtClasses = computed(() =>
+  props.active ? "font-semibold" : "font-normal"
+);
+</script>
