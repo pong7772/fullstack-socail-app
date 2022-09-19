f13935926e4d06e54889cefcf8ef390ece12d912
pong7772 <roth.samnangvisothipong19@kit.edu.kh>
2022-09-19 13:43:39 +0700

    update on main

--- components/composables/useTailwindConfig.js
+++ components/composables/useTailwindConfig.js
@@ -1,5 +1,6 @@
 export default () => {
     return {
-        deafaultTransitions: 'transition ease-in-out duration-400'
+        deafaultTransitions: 'transition ease-in-out duration-400',
+        twitterBorderColor: 'border-white-200 dark:border-gray-700'
     }
 }
