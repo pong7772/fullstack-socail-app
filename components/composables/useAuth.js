6522d45dfabfcaef36c4b09268ca9d230deb18d3
pong7772 <roth.samnangvisothipong19@kit.edu.kh>
2022-10-14 18:06:22 +0700

    working on posting

--- components/composables/useAuth.js
+++ components/composables/useAuth.js
@@ -1,6 +1,9 @@
+import useFetchApi from "./useFetchApi"
+import jwtDecode from "jwt-decode"
 export default ()=>{ 
     const useAuthToken = ()=> useState('auth_token)')
     const useAuthUser = ()=> useState('auth_user)')
+    const useAuthLoading=()=> useState('auth_loading',()=> true)
     const setToken = (newToken)=>{
         const authToken = useAuthToken()
         authToken.value= newToken
@@ -8,6 +11,11 @@ export default ()=>{
     const setUser = (newUser)=>{
         const authUser= useAuthUser()
         authUser.value= newUser
+    }
+    const setIsAuthLoading= (value)=>{
+        const authLoading = useAuthLoading()
+        authLoading.value= value
+
     }
 const login = ({ username, password }) => {
         return new Promise(async (resolve, reject) => {
@@ -40,20 +48,51 @@ const refreshToken = () => {
             }
         })
     }
+const getUser = () => {
+        return new Promise(async (resolve, reject) => {
+            try {
+                const data = await useFetchApi('/api/auth/user')
+                setUser(data.user)
+                resolve(true)
+            } catch (error) {
+                reject(error)
+            }
+        })
+    }
+    const reRefreshAccessToken = ()=>{
+        const authToken = useAuthToken()
+        if(!authToken.value){
+            return
+        }
+        const jwt = jwtDecode(authToken.value)
+        const newRefreshTime= jwt.exp - 60000;
+        setTimeout(async()=>{
+            await refreshToken()
+            reRefreshAccessToken()
+        }, newRefreshTime)
+    }
+
     const initAuth = ()=>{
         return new Promise(async(resolve, reject)=>{
+            setIsAuthLoading(true)
             try {
             await refreshToken()
+            await getUser()
+            reRefreshAccessToken()
             resolve(true)
             } catch (error) {
                 console.log(error) 
                 reject(error)
+            }finally{
+                setIsAuthLoading(false)
             }
         })
     }
     return {
         login,
         useAuthUser,
-        initAuth
+        initAuth,
+        useAuthToken,
+        useAuthLoading
     }
 }
---
+++ server/api/user/tweets/index.post.js
@@ -0,0 +1,7 @@
+export default defineEventHandler(async(event)=>{
+
+
+    return{
+        hello: 'hello'
+    }
+})
