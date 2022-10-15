a7c8eba35a7712b0ce70155d14ddda75d3ffa1eb
pong7772 <roth.samnangvisothipong19@kit.edu.kh>
2022-10-15 11:10:55 +0700

    update on post

--- server/api/user/tweets/index.post.js
+++ server/api/user/tweets/index.post.js
@@ -1,7 +1,30 @@
+import formidable from 'formidable'
+import { createTweet } from '~~/server/db/tweets'
+import { tweetTransformer } from '~~/server/transformers/tweet'
 export default defineEventHandler(async(event)=>{
 
+   const form = formidable({})
+
+    const response = await new Promise((resolve, reject) => {
+        form.parse(event.req, (err, fields, files) => {
+            if (err) {
+                reject(err)
+            }
+            resolve({ fields, files })
+        })
+    })
+
+    const {fields, files} =response
+    const userId = event.context?.auth?.user?.id
+
+    const tweetData= {
+        text: fields.text,
+        authorId: userId,
+
+    }
+     const tweet = await createTweet(tweetData)
 
     return{
-        hello: 'hello'
+        tweet: tweetTransformer(tweet)
     }
 })
